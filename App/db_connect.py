cac80ae6dc3074268ea0c61e98daa444552ca2d9
steven <stevenlin316@gmail.com>
2024-04-13 01:03:52 -0400

    init db_connect & test
    test is bugging!

---
+++ App/db_connect.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+# Used to connect to database
+class DatabaseConnect:
+    def __init__(self, dbname, user, password, host, port):
+        self.conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+    def get_conn(self):
+        return self.connection
+
+db = DatabaseConnect('3005_Project', 'postgres', '!postgres', 'localhost', '5432')
